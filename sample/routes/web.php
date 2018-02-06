d88066d3dd33a630bfe701436eb2e3246f8a0f2e
kaen98 <846969893@qq.com>
2018-02-06 15:54:33 +0800

    Finish layout and routes

--- sample/routes/web.php
+++ sample/routes/web.php
@@ -15,6 +15,9 @@
 //     return view('welcome');
 // });
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+// 注册
+Route::get('signup', 'UsersController@create')->name('signup');
